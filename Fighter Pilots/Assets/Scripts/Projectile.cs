3a776fbb1ee0bf40c43deed441fe32a1aa0fa272
Michael Allan <mb.allan@ymail.com>
2019-08-16 00:03:45 -0700

    Added twin guns to ship

---
+++ Fighter Pilots/Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField] private float damage;
+    [SerializeField] private float speed;
+
+    void FixedUpdate() {
+        transform.position += transform.forward * speed;
+    }
+}
---
+++ Fighter Pilots/Assets/Scripts/ShipWeapon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipWeapon : MonoBehaviour {
+    [HideInInspector] private float rightDelay;
+    [HideInInspector] private float leftDelay;
+
+    [SerializeField] private Transform rightWeapon;
+    [SerializeField] private Transform leftWeapon;
+    [SerializeField] private GameObject projectile;
+
+    [SerializeField] private float fireRate;
+
+    void Start() {
+        rightDelay = 0f;
+        leftDelay = 0f;
+    }
+
+    void Update() {
+        bool fireRight = Input.GetButton("Fire Right Weapon");
+        bool fireLeft = Input.GetButton("Fire Left Weapon");
+
+        if(fireRight && rightDelay <= 0f) {
+            GameObject createdProj = Instantiate(projectile, rightWeapon.position, Quaternion.identity);
+            createdProj.transform.forward = rightWeapon.forward;
+            rightDelay = 1f / fireRate;
+        }
+
+        if(fireLeft && leftDelay <= 0f) {
+            GameObject createdProj = Instantiate(projectile, leftWeapon.position, Quaternion.identity);
+            createdProj.transform.forward = leftWeapon.forward;
+            leftDelay = 1f / fireRate;
+        }
+
+        rightDelay = rightDelay > 0f ? rightDelay - Time.deltaTime : 0f;
+        leftDelay = leftDelay > 0f ? leftDelay - Time.deltaTime : 0f;
+    }
+}
