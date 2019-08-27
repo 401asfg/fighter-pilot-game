8a99480c178d6664f6621afedb09cf2cab61e335
Michael Allan <mb.allan@ymail.com>
2019-08-26 20:19:02 -0700

    Added split screen based on player amount

--- Fighter Pilots/Assets/Scripts/GameManager.cs
+++ Fighter Pilots/Assets/Scripts/GameManager.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     [SerializeField] private PlayerSpawnPoint[] spawnPoints;
+    [SerializeField] private SplitScreenView[] splitScreenViews;
+
+    private GameObject[] players;
 
     void Start() {
-        spawnPoints[0].Spawn(0);
-        spawnPoints[1].Spawn(1);
-        spawnPoints[2].Spawn(2);
-        spawnPoints[3].Spawn(3);
+        players = new GameObject[2];
+
+        players[0] = spawnPoints[0].Spawn(0);
+        players[1] = spawnPoints[1].Spawn(1);
+
+        UpdateSplitscreen();
     }
+
+    public void UpdateSplitscreen() {
+        for(int i = 0; i < players.Length; i++) {
+            players[i].GetComponentInChildren<Camera>().rect = splitScreenViews[players.Length - 1].playerViews[i];
+        }
+    }
+}
+
+[System.Serializable]
+public class SplitScreenView {
+    public Rect[] playerViews;
 }
--- Fighter Pilots/Assets/Scripts/Objects/PlayerSpawnPoint.cs
+++ Fighter Pilots/Assets/Scripts/Objects/PlayerSpawnPoint.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerSpawnPoint : MonoBehaviour {
     [SerializeField] private GameObject player;
 
-    public void Spawn(int index) {
+    public GameObject Spawn(int index) {
         GameObject playerInst = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
         playerInst.transform.parent = null;
         playerInst.GetComponent<Player>().index = index;
+
+        return playerInst;
     }
 }
