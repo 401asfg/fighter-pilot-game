afd999976a1e1989e77e8d9f645a5dcf72653422
Michael Allan <mb.allan@ymail.com>
2019-09-02 01:12:33 -0700

    Added basic aiming

--- Fighter Pilots/Assets/Scripts/Objects/Weapon.cs
+++ Fighter Pilots/Assets/Scripts/Objects/Weapon.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour {
     [HideInInspector] private bool canShoot;
+
     [SerializeField] private GameObject projectile;
     [SerializeField] private float shootDelay;
+
+    [SerializeField] private Transform mount;
+    [SerializeField] private float maxAngle;
 
     void Start() {
         canShoot = true;
     }
 
-    public void Shoot() {
+    public bool Shoot() {
         if(canShoot) {
             GameObject createdProj = Instantiate(projectile, transform.position, Quaternion.identity);
             createdProj.transform.forward = transform.forward;
             canShoot = false;
 
             StartCoroutine(AllowShooting());
+
+            return true;
         }
+
+        return false;
     }
 
     private IEnumerator AllowShooting() {
         yield return new WaitForSeconds(shootDelay);
         canShoot = true;
+    }
+
+    public bool Aim(Vector3 direction) {
+        float angle = Vector3.Angle(mount.forward, direction);
+
+        if (angle <= maxAngle) {
+            transform.forward = direction;
+            return true;
+        }
+
+        transform.forward = mount.forward;
+        return false;
     }
 }
--- Fighter Pilots/Assets/Scripts/Ship Control/ShipWeaponControl.cs
+++ Fighter Pilots/Assets/Scripts/Ship Control/ShipWeaponControl.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class ShipWeaponControl : MonoBehaviour {
     [SerializeField] private Player player;
+    [SerializeField] private Transform mainCamera;
 
     [SerializeField] private Weapon rightWeapon;
     [SerializeField] private Weapon leftWeapon;
 
     void Update() {
+        //Input
         bool fireRight = Input.GetButton("Fire Right Weapon " + player.index);
         bool fireLeft = Input.GetButton("Fire Left Weapon " + player.index);
 
-        if(fireRight) {
+        //Shoot
+        if (fireRight) {
             rightWeapon.Shoot();
         }
 
-        if(fireLeft) {
+        if (fireLeft) {
             leftWeapon.Shoot();
         }
+
+        //Aim
+        bool canUseRight = rightWeapon.Aim(mainCamera.forward);
+        bool canUseLeft = leftWeapon.Aim(mainCamera.forward);
     }
 }
