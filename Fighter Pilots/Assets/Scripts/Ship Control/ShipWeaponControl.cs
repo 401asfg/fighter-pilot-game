57432f7cf82261b07e0f041e24218e72a4ef9bf8
Michael Allan <mb.allan@ymail.com>
2019-09-07 15:58:46 -0700

    Improved crosshair reactions to aiming and movement

--- Fighter Pilots/Assets/Scripts/Ship Control/ShipWeaponControl.cs
+++ Fighter Pilots/Assets/Scripts/Ship Control/ShipWeaponControl.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipWeaponControl : MonoBehaviour {
+    //Ship Component References
     [SerializeField] private Player player;
-    [SerializeField] private Transform mainCamera;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform aimingBody;
+    [SerializeField] private RectTransform crosshair;
 
+    //Weapon References
     [SerializeField] private Weapon rightWeapon;
     [SerializeField] private Weapon leftWeapon;
 
-    [SerializeField] private RectTransform crosshair;
+    //Aiming Parameters
     [SerializeField] private LayerMask aimRayLayers;
     [SerializeField] private float aimRayDistance;
+    [SerializeField] private float crosshairAcceleration;
 
     void Update() {
         //Input
@@ -28,22 +33,23 @@ public class ShipWeaponControl : MonoBehaviour {
         }
 
         //Aim
-        Ray ray = new Ray(mainCamera.position, mainCamera.forward);
+        Ray ray = new Ray(aimingBody.position, aimingBody.forward);
         RaycastHit hit;
-
         Vector3 target;
-        Camera cam = mainCamera.GetComponent<Camera>();
 
         if(Physics.Raycast(ray, out hit, aimRayDistance, aimRayLayers)) {
             target = hit.point;
         }
 
         else {
-            target = (aimRayDistance * mainCamera.forward + mainCamera.position);
+            target = (aimRayDistance * aimingBody.forward + aimingBody.position);
         }
 
         rightWeapon.Aim(target - rightWeapon.transform.position);
         leftWeapon.Aim(target - leftWeapon.transform.position);
-        crosshair.position = cam.WorldToScreenPoint(target);
+
+        if(cam.gameObject != aimingBody.gameObject) {
+            crosshair.position = Vector3.Lerp(crosshair.position, cam.WorldToScreenPoint(target), Time.deltaTime * crosshairAcceleration);
+        }
     }
 }
